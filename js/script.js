6f58fcb209e17f215da4c5d4dfc3ba59d3ca96de
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2022-09-28 17:39:35 -0700

    feat to checkbox if toggle then a line through will occur in list item

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 
-
     const inputEl = document.getElementById('input');
     const listCont = document.getElementById('listContainer');
     const submitEl = document.getElementById('submit');
     const clearEl = document.getElementById('clear');
-
-    inputEl.placeholder = 'type...';
+    const box = document.querySelectorAll('.line-checkbox');
 
     submitEl.addEventListener('click', () => {
+        if (inputEl.value === '') {
+            alert('type something to do'); 
+        } else {
+        // create p element
         let item = document.createElement('p');
+        // turning list item to text
         item.innerText = inputEl.value;
-        listCont.appendChild(item);
+        // blank string after submitting
         inputEl.value = '';
-        listCont.className = 'list-container';
+        // appending checkbox to every list item
+        inputEl.placeholder = 'type...';
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        item.appendChild(checkbox);
-
+        
+        //create new div element 
+        let div = document.createElement('div');
+        div.appendChild(checkbox);
+        div.appendChild(item);
+        listCont.appendChild(div);
+        div.className = 'list-Cont';
+        checkbox.className = 'line-checkbox';
+        }
+        
     });
+    
+    // clears list after toggling 
     clearEl.addEventListener('click', () => {
-        listCont.innerHTML = '';
+        let itemCont = document.getElementsByClassName('list-Cont');
+        for (const item of itemCont) {
+            item.innerHTML = '';
+        }
+    
     })
+
